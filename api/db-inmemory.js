26efa49f229801287744e1b3e48945d1887c127c
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2019-12-02 00:42:50 +0000

    all working !!

--- api/db-inmemory.js
+++ api/db-inmemory.js
@@ -11,48 +11,53 @@ module.exports.randomNumber = async (id) => {
 module.exports.roles = (email) => {
   for (let i = 0; i < users.length; i++) {
     if (users[i].email == email) {
-      return (users[i].roles);
-    }else{
-      return [];
+      return users[i].roles;
     }
   }
+  return [];
 };
 
 //request access
 module.exports.accessRequest = async (email) => {
     for (var i = 0; i < users.length; i++) {
         if (users[i].email === email) {
-          console.log("alreasy have access")
+          return 202
         }
         else{
-            request.email = email
-            console.log("request sent")
-            console.log(email)
-            return 202
+          preparedEntry = {"email": email, "roles": []}
+          request.push(email)
+          users.push(preparedEntry)
+          return 202
         }
       }
-     console.log("request");
 };
 
 //get user list
 module.exports.userList = async () => {
-  console.log(users)
   return users;
 };
 
 // get user request list
 module.exports.userRequest = async () => {
-  return request
+  return request;
 };
 
-module.exports.aprove = async () => {
-    console.log("aprove");
+module.exports.aprove = async (email) => {
+  let reqIndex = request.indexOf(email);
+  request.splice(reqIndex, 1);
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].email === email) {
+      users[i].roles = ['user'];
+      return users[i];
+    }
+  }
+  return [];
 };
 
 module.exports.delete = async (email) => {
-    for (var i = 0; i < user.length; i++) {
-        if (user[i].email === email) {
-          user[i].role.pop;
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].email === email) {
+          users.splice(i, 1);
         }
       }
 };
--- api/index.js
+++ api/index.js
@@ -8,8 +8,7 @@ let db = require('./db-inmemory')
 api.get('/random', async (req, res) => {
   try {
     let role = db.roles(req.user.emails[0].value)
-    console.log(role);
-    if(role.includes("admin") ){
+    if(role.includes("admin") || role.includes("user") ){
       res.set('content-type', 'text/plain');
       res.status(200).json(await db.randomNumber());
     }else{
@@ -33,7 +32,7 @@ api.get('/user/roles', async (req, res) => {
 api.post('/user/request', async (req, res) => {
   try {
     res.set('content-type', 'application/json');
-    res.send(await db.accessRequest(req.user.emails[0].value));
+    res.status(202).json(await db.accessRequest(req.user.emails[0].value));
   } catch (e) {
     console.error(e);
     res.sendStatus(500);
@@ -68,22 +67,32 @@ api.get('/user/request', async (req, res) => {
   }
 });
 
-api.post('/user/aprove', bodyParser.text(), (req, res) => {
+//-------------------------------------------------------------
+api.post('/user/approve', bodyParser.text(), async (req, res) => {
   try {
-    let id = "2"
-    res.send(db.aprove(id));
-  } catch (e) {
-    console.error(e);
+    if (db.roles(req.user.emails[0].value).includes("admin")) {
+      res.set('content-type', 'application/json');
+      res.status(200).json(await db.aprove(req.body));
+    } else {
+      res.sendStatus(403);
+    }
+  }
+  catch (e) {
     res.sendStatus(500);
   }
 });
 
-api.delete('/user/user1@a.b', bodyParser.text(), (req, res) => {
+
+api.delete('/user/:email', bodyParser.text(), (req, res) => {
   try {
-    let id = "2"
-    res.send(db.delete(id));
-  } catch (e) {
-    console.error(e);
+    if (db.roles(req.user.emails[0].value).includes("admin")) {
+      db.delete(req.params.email);
+      res.sendStatus(204)
+    } else {
+      res.sendStatus(403);
+    }
+  }
+  catch (e) {
     res.sendStatus(500);
   }
 });
