b97c22c5de79c53fac245907d131fe7bfa4966de
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2019-12-04 11:51:17 +0000

    updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const GoogleAuth = require('simple-google-openid');
 app.use(express.static('static', { extensions: ['html'] }));
-app.use(GoogleAuth('849096798416-3fcs1gjvb3jfn95fb4bisl3j1fajdaaf.apps.googleusercontent.com'));
+app.use(GoogleAuth('849096798416-71hedeb8diorogkd025e449g55oeo72p.apps.googleusercontent.com'));
 // return 'Not authorized' if we don't have a user
 app.use('/api', GoogleAuth.guardMiddleware(),require('./api'));
 
